4bb29ac5fd5be81b30107594fd24a5acb8353715
wangtong1201 <1311669275@qq.com>
2021-01-01 23:47:23 +0800

    图书适配器

--- java/com/xinhua/bookstore/BookAdapter.java
+++ java/com/xinhua/bookstore/BookAdapter.java
@@ -3,7 +3,11 @@ package com.xinhua.bookstore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.xinhua.bookstore.Table.Book;
@@ -14,14 +18,50 @@ public class BookAdapter extends RecyclerView.Adapter<BookAdapter.ViewHolder> {
     private List<Book> BookList;
 
     static class ViewHolder extends RecyclerView.ViewHolder{
+        View bookView;
+        TextView book_name;
+        TextView book_author;
+        TextView book_price;
+        ImageView book_image;
+        public ViewHolder(View view)
+        {
+            super(view);
+            bookView = view;
+            book_name = view.findViewById(R.id.book_name);
+            book_author = view.findViewById(R.id.book_author);
+            book_price = view.findViewById(R.id.book_price);
+            book_image=view.findViewById(R.id.book_image);
+        }
 
+    }
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Book book=BookList.get(position);
+        holder.book_price.setText(String.valueOf(book.getPrice()));
+        holder.book_name.setText(book.getName());
+        holder.book_author.setText(book.getAuthor());
+        holder.book_image.setImageResource(book.getImageId());
+    }
+    public int getItemCount() {
+        return BookList.size();
     }
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.book_item,parent,false);
         final  ViewHolder holder=new ViewHolder(view);
-        holder.bookview.setOnClickListener(new View.OnClickListener())
+        holder.bookView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                    int position=holder.getAdapterPosition();
+                    Book book=BookList.get(position);
+                    Toast.makeText(v.getContext(),"you click view"+book.getName(),
+                            Toast.LENGTH_SHORT).show();
+            }
+        });
 
+            return  holder;
+    }
+    public BookAdapter(List<Book> bookList) {
+        BookList = bookList;
     }
 }
