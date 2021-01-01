3e81494aac8470a5d44672a06b210ae787067ff1
xzy1799603358 <2933278219@qq.com>
2021-01-01 21:36:57 +0800

    xzy test

---
+++ java/com/xinhua/bookstore/BookContent.java
@@ -0,0 +1,14 @@
+package com.xinhua.bookstore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BookContent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_content);
+    }
+}
