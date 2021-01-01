3593a484bfe10ce3decf73d440f6ab4d5b7967e8
wangtong1201 <1311669275@qq.com>
2021-01-01 22:22:55 +0800

    图书列表

---
+++ java/com/xinhua/bookstore/BookAdapter.java
@@ -0,0 +1,20 @@
+package com.xinhua.bookstore;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.xinhua.bookstore.Table.Book;
+
+import java.util.List;
+
+public class BookAdapter extends RecyclerView.Adapter<BookAdapter.ViewHolder> {
+private List<Book> BookList;
+@Override
+    public ViewHolder onCreateViewHolder(ViewGroup,int viewType)
+{
+    View view= LayoutInflater.from()
+}
+}
