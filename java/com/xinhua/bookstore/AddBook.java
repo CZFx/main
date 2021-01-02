62a3b7331aec3be4981b425ca2f929c3bee59148
wangtong1201 <1311669275@qq.com>
2021-01-02 13:52:11 +0800

    图书适配器

---
+++ java/com/xinhua/bookstore/AddBook.java
@@ -0,0 +1,18 @@
+package com.xinhua.bookstore;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.litepal.crud.DataSupport;
+
+public class AddBook extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.addbook);
+
+
+    }
+}
