5eead60f42e65abcf7600473d7bd1730064eb376
pyqjw <1847862214@qq.com>
2021-01-02 17:17:20 +0800

    qjw added

--- java/com/xinhua/bookstore/ui/login/LoginActivity.java
+++ java/com/xinhua/bookstore/ui/login/LoginActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +13,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.xinhua.bookstore.MainActivity;
 import com.xinhua.bookstore.R;
 import com.xinhua.bookstore.RegisterActivity;
+import com.xinhua.bookstore.Table.User;
+
+import org.litepal.crud.DataSupport;
+
+import java.util.List;
 
 public class LoginActivity extends AppCompatActivity {
     @Override
@@ -23,7 +30,16 @@ public class LoginActivity extends AppCompatActivity {
         blogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                EditText username, password;
+                username = findViewById(R.id.login_username);
+                password = findViewById(R.id.login_password);
+
+                List<User> userList = DataSupport.where("username = ?", username.getText().toString()).find(User.class);
+                if(userList.size() != 0 && userList.get(0).getPassword().equals(password.getText().toString())) {
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                } else {
+                    Toast.makeText(getApplicationContext(), "用户名或密码错误", Toast.LENGTH_SHORT).show();
+                }
             }
         });
         bregister.setOnClickListener(new View.OnClickListener() {
